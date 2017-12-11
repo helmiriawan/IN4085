7611b00a932e226628e6c47f272d5306cfc20402
helmiriawan <helmiriawan@student.tudelft.nl>
2017-12-11 21:57:56 +0100

    Add the benchmark for scenario 2

---
+++ scenario2/benchmark.m
@@ -0,0 +1,19 @@
+% Specify number of objects for training set (200-1000)
+number_of_objects = 10;
+
+% Retrieve NIST data file
+data_file = prnist(0:9, 1:number_of_objects);
+
+% Pre-process and convert data file to data set
+data_set = my_rep(data_file);
+
+% Compute the classifier
+start = clock;
+classifier = svc(data_set, 'radial_basis', 4);
+finish = clock;
+
+% Evaluate the error classification
+error = nist_eval('my_rep', classifier, 10);
+
+% Show training time
+fprintf('%2.3f s\n', etime(finish, start));
