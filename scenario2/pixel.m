25d1cf9339e96d8adbe0a980af7273ee1529e611
helmiriawan <helmiriawan@student.tudelft.nl>
2018-01-29 21:26:36 +0100

    Modify approach on feature reduction

--- scenario2/pixel.m
+++ scenario2/pixel.m
@@ -39,7 +39,7 @@ gridsearch(data_set, [], 'cross-validation', 'svc', 1:10, [], 'radial_basis')
 
 % Feature reduction %
 
-% Load NIST data file
+% Data preparation
 data_file = prnist(0:9, 1:10);
 data_set = data_preparation(data_file, 20);
 
@@ -50,17 +50,6 @@ selected_features = featself(data_set, 'eucl-s', n);
 finish = clock;
 fprintf('%2.3f s\n', etime(finish, start));
 
-% Plot the feature curve
-feature_curve(data_set*selected_features, [20:20:200])
-
-% Evaluate with cross validation
-crossvalidation(data_set*selected_features, 5, 10)
-
-% k-Nearest Neighbor
-gridsearch(data_set*selected_features, [], 'cross-validation', 'knnc', 1:10)
-
-% Back-propagation Feed-forward Neural Network
-gridsearch(data_set*selected_features, [], 'cross-validation', 'bpxnc', 5:5:50, 2000)
-
-% Support Vector Machine 
-gridsearch(data_set*selected_features, [], 'cross-validation', 'svc', 1:10, [], 'radial_basis')
+% Generate feature curve
+feature_curve = clevalf(data_set*selected_features, svc([], 'radial_basis', 4), [20:20:200], 0.4, 5);
+plote(feature_curve, [], [], [], 'noapperror');
